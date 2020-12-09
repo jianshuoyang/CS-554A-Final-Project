c6e3ee494acde4bce052e7910abc1b3cf7d1b5be
Jianshuo Yang <jyang63@stevens.edu>
2020-12-09 17:07:26 -0500

    .

--- client/src/App.js
+++ client/src/App.js
@@ -1,35 +1,14 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import Home from './components/Home';
-import UserProfile from './components/UserProfile';
-
-
-
-
+import test from "./components/test";
 
 function App() {
 
-  if (window.location.href.includes("access_token")) {
-    return (
-      <UserProfile />
-        
-    );
-  }
-
   return (
-      <Router>
-        <div>
-          <header className="App-header">
-            <h1 className="App-title">
-            Welcome To Our Music Website
-            </h1>
-            
-          </header>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/userprofile" component={UserProfile} />
-        </div>
-      </Router>
+      <div>
+        <test/>
+      </div>
   );
 }
 
