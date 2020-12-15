72e985e4e55df1c741470a803ca2f66aa5e3fa04
jasperP <53663744+jasperpwj@users.noreply.github.com>
2020-12-14 19:27:34 -0500

    Update removeSong.js

--- client/src/actions/removeSong.js
+++ client/src/actions/removeSong.js
@@ -8,6 +8,7 @@ export default async function removeSong(songId) {
     };
     try {
         await axios.post('http://localhost:5000/users/removeSong', songInfo);
+        await axios.post('http://localhost:5000/songs/removeASong', songInfo);
         return true;
     } catch(e) {
         console.log({error:e})
